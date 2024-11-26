ef80d2ae1affe18a7a1e2d32074d71bc9503b10e
Mersey05 <magtotojersey@gmail.com>
2024-11-26 09:41:49 +0800

    Add files via upload

--- scripts/homePage/home.js
+++ scripts/homePage/home.js
@@ -2,7 +2,8 @@ const nav = document.querySelector(".mainNav");
 let isScrolling;
 
 window.addEventListener("scroll", () => {
-  nav.classList.add("hidden"); n
+  nav.classList.add("hidden");
+  n;
 
   clearTimeout(isScrolling);
 
@@ -12,5 +13,39 @@ window.addEventListener("scroll", () => {
 });
 
 window.addEventListener("mousemove", () => {
-  nav.classList.remove("hidden"); 
+  nav.classList.remove("hidden");
 });
+
+window.addEventListener("load", () => {
+  document.querySelector(".homeCenter").classList.add("animate-center");
+  document.querySelector(".left-drawing").classList.add("animate-left");
+  document.querySelector(".right-drawing").classList.add("animate-right");
+});
+
+const animateOnScroll = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add(entry.target.dataset.animationClass);
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const observer = new IntersectionObserver(animateOnScroll, {
+  threshold: 0.05,
+});
+
+document.querySelectorAll(".animate-on-scroll").forEach((element) => {
+  observer.observe(element);
+});
+
+// function adjustFacebookWidth() {
+//   const fbPageElement = document.querySelector(".fb-page"); // Replace with your actual element selector
+//   if (window.innerWidth < 550) {
+//     fbPageElement.setAttribute("data-width", "400");
+//   }
+// }
+
+// // Adjust width on load and on resize
+// window.addEventListener("load", adjustFacebookWidth);
+// window.addEventListener("resize", adjustFacebookWidth);
